20250e808f7dabf04fd0c135b749eab782af8cbc
AGoodPyDev <104173623+AGoodPyDev@users.noreply.github.com>
2022-04-21 18:05:32 -0500

    Update PythonFile2.py

--- Python/PythonFile2.py
+++ Python/PythonFile2.py
@@ -1,16 +1,26 @@
 # Just a quick script before bed.
 
-x = int(input("Enter a number: "))
-y = int(input("Enter another number: "))
-z = x + y
+# Importing "os" allows a line of code to reset the script, importing time will allow us to delay lines of code
+import os
+import sys
+import time
 
-print(z)
+# Functions are great! Use them more! -PyDev
+def restart():
+    print("Restarting...")
+    time.sleep(5)
+    os.execv(sys.executable, ['python'] + sys.argv)
+    
+x = int(input("\nEnter a number: "))
+y = int(input("Enter another number: "))
+# Removing useless code. -Pydev
+print(x+y)
 
 a = input("Do you want to restart the program? ")
-if a == "yes":
-    print("Restarting the program...")
-    import PythonFile2
-elif a == "no":
+if a in [ "yes", "y" ]: # if a == "yes" wont work.
+    restart()
+    # I don't know why you tried to import your own file??? -PyDev
+elif a in [ "no", "n" ]:
     print("Ending the program...")
 else:
     print("Invalid input. Ending the program...")
